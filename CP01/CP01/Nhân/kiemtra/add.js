e26418a2e5ed84a3aff9c5b07f4bdb837432fede
diplamtuan <85506794+diplamtuan@users.noreply.github.com>
2023-11-19 09:30:00 +0700

    Buoi hoc so 6

---
+++ CP01/CP01/Nhân/kiemtra/add.js
@@ -0,0 +1,12 @@
+let list =document.querySelectorAll('.list.item')
+
+localStorage.getItem('login', true)
+
+// oneclick = function() {
+//     alert("bạn chưa login")
+// }
+// if (condition) {
+    
+// } else {
+    
+// }
